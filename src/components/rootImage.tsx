d4f08122a91bae4d5c188e501f9bea32a0ddc77c
Nicknul <jea7730@gmail.com>
2024-07-12 00:14:51 +0900

    :triangular_flag_on_post: 정호연 : Root 이미지 모듈

---
+++ src/components/rootImage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { styles } from '../styles/styles';
+
+const RootImage: React.FC = () => {
+  return <div style={styles.rootImage}></div>;
+};
+
+export default RootImage;
