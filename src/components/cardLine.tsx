9a7f23c9491e8f19e9bc494369cf1f4140069ce8
Nicknul <jea7730@gmail.com>
2024-07-12 00:16:29 +0900

    :triangular_flag_on_post: 정호연 : card-line module

---
+++ src/components/cardLine.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { styles } from '../styles/styles';
+
+const CardLine: React.FC = () => {
+  return <div style={styles.cardLine}></div>;
+};
+
+export default CardLine;
